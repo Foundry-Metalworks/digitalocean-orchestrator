fb8edfdd50c67bddc935d7da3519d58d0ddce8e9
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2023-01-04 01:03:11 +0100

    remove admin scope requirements, to be replaced later

--- src/routes/digitalocean.ts
+++ src/routes/digitalocean.ts
@@ -1,23 +1,14 @@
 import express from "express";
 import dropletsController from "../controllers/droplets";
 import snapshotsController from "../controllers/snapshots";
-import { requiredScopes } from "express-oauth2-jwt-bearer";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const routes = express.Router();
-routes.post(
-  "/start",
-  requiredScopes("admin"),
-  dropletsController.onStartRequest
-);
-routes.post("/stop", requiredScopes("admin"), dropletsController.onStopRequest);
-routes.post(
-  "/save",
-  requiredScopes("admin"),
-  snapshotsController.onSaveRequest
-);
+routes.post("/start", dropletsController.onStartRequest);
+routes.post("/stop", dropletsController.onStopRequest);
+routes.post("/save", snapshotsController.onSaveRequest);
 routes.get("/status", dropletsController.onStatusRequest);
 routes.get("/ip", dropletsController.onIPRequest);
 
